a00643405c881a51a546e4702c788e4eb6dc4634
Bapumafiyas <154887259+Bapumafiyas@users.noreply.github.com>
2024-09-13 20:28:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
 import pyrogram.utils
-pyrogram.utils.MIN_CHANNEL_ID = -your channel id
+pyrogram.utils.MIN_CHANNEL_ID = -your channel id -1002239557483
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
