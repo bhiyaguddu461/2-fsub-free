65fdab80df669aef342fb8284aedf87b04df5f88
bhiyaguddu461 <151850248+bhiyaguddu461@users.noreply.github.com>
2024-12-02 20:07:24 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "more":
         await query.message.edit_text(
-          text = f"<b>○ Creator : @brainsaga\n for more disturbin.com/collection</b>",
+          text = f"<b></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
