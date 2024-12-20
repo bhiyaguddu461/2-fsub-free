c99a332934a8a42565778581b79fe6f67b6b5cdb
bhiyaguddu461 <151850248+bhiyaguddu461@users.noreply.github.com>
2024-12-20 20:00:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
 import pyrogram.utils
-pyrogram.utils.MIN_CHANNEL_ID = -1002107245494
+pyrogram.utils.MIN_CHANNEL_ID = -1002167194798
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
